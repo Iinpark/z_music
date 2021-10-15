29b09029784536cd9a779000f2267af03ddb1922
Sultan <linpark11197@gmail.com>
2021-10-15 21:31:23 +0600

    подрубили плеер

---
+++ src/components/Player.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="player__wrapper">
+    <video
+      ref="videoPlayer"
+      class="video-js"
+    ></video>
+  </div>
+</template>
+
+<script>
+import videojs from "video.js";
+import "videojs-youtube";
+import "video.js/dist/video-js.css";
+
+export default {
+  name: "VideoPlayer",
+  props: {
+    options: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+  },
+  data() {
+    return {
+      player: null,
+    };
+  },
+  mounted() {
+    this.player = videojs(
+      this.$refs.videoPlayer,
+      {
+        techOrder: ["youtube"],
+        ...this.options,
+      },
+      function onPlayerReady() {
+        console.log("onPlayerReady", this);
+      }
+    );
+	window.player = this.player;
+  },
+  beforeDestroy() {
+    if (this.player) {
+      this.player.dispose();
+    }
+  },
+};
+</script>
+
+<style scoped>
+	.player__wrapper {
+		max-height: 200px;
+		height: 200px;
+	}
+</style>
---
+++ src/pages/MainPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Плеер</h1>
+    <Player :options="options" />
+  </div>
+</template>
+
+<script>
+import Player from '@/components/Player.vue'
+export default {
+  data () {
+    return {
+      options: {
+        sources:[{
+          type:'video/youtube',
+          src: 'https://www.youtube.com/watch?v=Tf1DEI2lEe0&t=2376s'
+        }]
+      }
+    }
+  },
+  components: {
+    Player
+  }
+}
+</script>
+
+<style>
+
+</style>

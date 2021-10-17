e88a98b8bf6fc9ae14d83efa593ef279d24de910
Sultan <linpark11197@gmail.com>
2021-10-17 15:52:42 +0600

    сделали .env

--- src/background.js
+++ src/background.js
@@ -3,8 +3,9 @@
 import { app, protocol, BrowserWindow } from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
+require('dotenv').config();
 const isDevelopment = process.env.NODE_ENV !== 'production'
-// import Telegraf from 'telegraf';
+
 
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([

da2a8013084cb05c3b38d247083260a4e90edc47
Sultan <linpark11197@gmail.com>
2021-10-17 20:57:38 +0600

    сommandHandler

--- src/utils/commandParser.js
+++ src/utils/commandParser.js
@@ -2,8 +2,16 @@
  * @param text {String} 
  * @param entites {Array} 
  */
-const parser = (text, entites) => {
-	return {text, entites}
+const parser = (text) => {
+	if (!text.startsWith('/')) {
+		throw new Error(`${text} не является командой`)
+	}
+
+	const tokens = text.split(' ');
+	return {
+		command: tokens[0].replace('/', ''),
+		payload: tokens[1]
+	}
 }
 
 export default parser;

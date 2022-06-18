a860a59bf731a7ad80c4b01fb3ee2f8e07b34b01
Sultan <linpark11197@gmail.com>
2022-06-19 00:25:02 +0600

    video id tests writed

--- src/utils/youtubeVideoIdExtractor.js
+++ src/utils/youtubeVideoIdExtractor.js
@@ -1,5 +1,6 @@
 export default (link) => {
 	// https://stackoverflow.com/questions/5830387/how-do-i-find-all-youtube-video-ids-in-a-string-using-a-regex
-	return new RegExp(/https?:\/\/(?:[0-9A-Z-]+\.)?(?:youtu\.be\/|youtube(?:-nocookie)?\.com\S*?[^\w\s-])([\w-]{11})(?=[^\w-]|$)(?![?=&+%\w.-]*(?:['"][^<>]*>|<\/a>))[?=&+%\w.-]*/ig).
-	exec(link)[1];
+	const regex = new RegExp(/https?:\/\/(?:[0-9A-Z-]+\.)?(?:youtu\.be\/|youtube(?:-nocookie)?\.com\S*?[^\w\s-])([\w-]{11})(?=[^\w-]|$)(?![?=&+%\w.-]*(?:['"][^<>]*>|<\/a>))[?=&+%\w.-]*/ig);
+	const processed = regex.exec(link);
+	return processed?.[1] || '';
 };
